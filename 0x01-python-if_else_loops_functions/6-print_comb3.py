7c213380fcef30874c2f654e863313dcc5abdcb3
LionelMv <mvangicode@gmail.com>
2022-09-24 11:56:36 +0300

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,5 @@ for digit1 in range(0, 9):
     for digit2 in range((digit1 + 1), 10):
         if digit1 == 8:
             print("{}{}".format(digit1, digit2))
-        print("{}{}, ".format(digit1, digit2), end="")
+        else:
+            print("{}{}, ".format(digit1, digit2), end="")
