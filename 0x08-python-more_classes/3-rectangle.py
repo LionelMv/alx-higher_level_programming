0c968c4830e6146ee77cb417167b75fa1e998c1b
LionelMv <mvangicode@gmail.com>
2023-04-04 01:08:19 +0300

    3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -10,30 +10,31 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """
-        Initializing the class.
+        Initializes a Rectangle instance.
+
         Args:
-            width: width of the rectangle
-            height: height of the rectangle
+            width (int): The width of the rectangle (default: 0).
+            height (int): The height of the rectangle (default: 0).
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Getter
-        Returns:
-            width of the rectangle
-        """
+        """Gets the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        Setter
+        Sets the width of the rectangle.
+
+        Args:
+            value (int): The width of the rectangle.
+
         Raises:
-            TypeError if width is not an integer
-            ValueError if width is negative
+            TypeError: if the value is not an integer.
+            ValueError: if the value is negative.
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -43,20 +44,20 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Getter
-        Return:
-            height of the rectangle
-        """
+        """Gets the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        Setter
+        Sets the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
         Raises:
-            TypeError if height is not an integer
-            ValueError if height is negative
+            TypeError: if height is not an integer.
+            ValueError: if height is negative
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
@@ -69,19 +70,13 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
-        """
-        Returns the perimeter of the Rectangle.
-        If width or height = 0, return 0.
-        """
+        """Returns the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
-        """
-        Returns a rectangle of #s.
-        Returns an empty string if width or height = 0.
-        """
+        """Returns a # string representation of a rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         rect_hashes = "\n".join(["#" * self.__width
