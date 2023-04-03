6b2b523610be1c84cbbfc514e0599570f017f372
LionelMv <mvangicode@gmail.com>
2023-04-04 01:11:15 +0300

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,30 +10,31 @@ class Rectangle:
     """
     def __init__(self, width=0, height=0):
         """
-        Initializing the class.
+        Initializes a Rectangle instance.
+
         Args:
-            width: width of the rectangle
-            height: height of the rectangle
+            width (int): The width of the rectangle (default: 0).
+            height (int): The height of the rectangle (default: 0).
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Getter
-        Returns:
-            width of the rectangle
-        """
+        """Gets the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        Setter
+        Sets the width of the rectangle.
+
+        Args:
+            value (int): The width of the rectangle.
+
         Raises:
-            TypeError if width is not an integer
-            ValueError if width is negative
+            TypeError: if the value is not an integer.
+            ValueError: if the value is negative.
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -43,20 +44,20 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Getter
-        Return:
-            height of the rectangle
-        """
+        """Gets the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        Setter
+        Sets the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
         Raises:
-            TypeError if height is not an integer
-            ValueError if height is negative
+            TypeError: if height is not an integer.
+            ValueError: if height is negative
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
