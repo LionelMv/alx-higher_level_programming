330cf7255574d290811f7897d6784b4e90183a43
LionelMv <mvangicode@gmail.com>
2023-04-04 01:01:14 +0300

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -6,19 +6,20 @@ Module defines a rectangle
 
 class Rectangle:
     """
-    Class represents a rectangle
+    Class represents a rectangle.
+
     Args:
-        number_of_instances: each time a new rectangle is created
-            or deleted.
+        number_of_instances (int): The number of Rectangle instances created.
     """
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """
-        Initializing the class.
+        Initializes a Rectangle instance.
+
         Args:
-            width: width of the rectangle
-            height: height of the rectangle
+            width (int): The width of the rectangle (default: 0).
+            height (int): The height of the rectangle (default: 0).
         """
         self.width = width
         self.height = height
@@ -27,20 +28,20 @@ class Rectangle:
 
     @property
     def width(self):
-        """
-        Getter
-        Returns:
-            width of the rectangle
-        """
+        """Gets the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
         """
-        Setter
+        Sets the width of the rectangle.
+
+        Args:
+            value (int): The width of the rectangle.
+
         Raises:
-            TypeError if width is not an integer
-            ValueError if width is negative
+            TypeError: if the value is not an integer.
+            ValueError: if the value is negative.
         """
         if type(value) is not int:
             raise TypeError("width must be an integer")
@@ -50,20 +51,20 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Getter
-        Return:
-            height of the rectangle
-        """
+        """Gets the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
         """
-        Setter
+        Sets the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
         Raises:
-            TypeError if height is not an integer
-            ValueError if height is negative
+            TypeError: if height is not an integer.
+            ValueError: if height is negative
         """
         if type(value) is not int:
             raise TypeError("height must be an integer")
@@ -76,19 +77,14 @@ class Rectangle:
         return self.__height * self.__width
 
     def perimeter(self):
-        """
-        Returns the perimeter of the Rectangle.
-        If width or height = 0, return 0.
-        """
+        """Returns the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__height + self.__width)
 
     def __str__(self):
         """
-        Returns a rectangle of #s.
-        Returns an empty string if width or height = 0.
-        """
+        Returns a # string representation of a rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         rect_hashes = "\n".join(["#" * self.__width
@@ -96,11 +92,11 @@ class Rectangle:
         return rect_hashes
 
     def __repr__(self):
-        "Returns the string representation of the Rectangle"
+        """Returns the string representation of the rectangle instance."""
         return f"Rectangle({self.__width}, {self.__height})"
 
     def __del__(self):
-        """ Deletes instance of class """
+        """Deletes an instance of a Rectangle."""
         print("Bye rectangle...")
 
         Rectangle.number_of_instances -= 1
