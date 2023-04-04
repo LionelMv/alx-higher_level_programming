2e72f4c12384e9c4f1dbe78ffc75ea583a4dd25b
LionelMv <mvangicode@gmail.com>
2023-04-04 22:40:41 +0300

    removed __init__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,6 +4,7 @@ Defines class with no class or object attribute
 Control dynamically created instance attributes
 """
 
+
 class LockedClass:
     """
     Prevents user from creating new instance attribute dynamically
@@ -12,11 +13,11 @@ class LockedClass:
 
     __slot__ = "first_name"
 
-    def __init__(self, first):
-        """
-        Initializes the class to only instantiate first_name
-        
-        Args:
-        first_name (str)
-        """
-        self.first_name = first
+    # def __init__(self, first):
+    #     """
+    #     Initializes the class to only instantiate first_name
+
+    #     Args:
+    #     first_name (str)
+    #     """
+    #     self.first_name = first
