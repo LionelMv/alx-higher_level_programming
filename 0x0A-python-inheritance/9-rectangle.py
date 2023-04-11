ae501dbd193c17f6fa3fd2dd1e1eb41afb02df23
LionelMv <mvangicode@gmail.com>
2023-04-12 02:12:21 +0300

    9-rectangle.py

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module 9-rectangle
-Contains a Rectangle class.
+Defines a class Rectangle that inherits from BaseGeometry.
 """
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
