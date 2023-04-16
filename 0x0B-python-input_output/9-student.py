e63a72d567f75fa5f6eac7194cdb7353f9dfa2a9
LionelMv <mvangicode@gmail.com>
2023-04-17 00:07:26 +0300

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 11-student
+Represents student name and age.
+"""
+
+
+class Student():
+    """Class represents student."""
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes student.
+        Args:
+            first_name: first name of student.
+            last_name: last name of student.
+            age: age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns dictionary description with simple data structure
+        (list, dictionary, dictionary, string)
+        for JSON serialization of an object
+        """
+        return self.__dict__
