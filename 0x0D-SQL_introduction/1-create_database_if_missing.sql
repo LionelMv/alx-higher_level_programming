f29901043c6416d1f9152873c1f7ffe5655b3968
LionelMv <mvangicode@gmail.com>
2023-05-18 01:30:30 +0300

    added the command used

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,4 @@
 -- create database; shouldn't fail if non-existent
+-- cat 1-create_database_if_missing.sql | mysql -hlocalhost -uroot -p
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,4 @@
 -- delete database; shouldn't fail if non-existent
+-- cat 0-list_databases.sql | mysql -hlocalhost -uroot -p
 
 DROP DATABASE IF EXISTS hbtn_0c_0;
