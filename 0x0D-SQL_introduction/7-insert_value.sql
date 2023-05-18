4fd1e667c7390086215cdc256b81b598313c2bb5
LionelMv <mvangicode@gmail.com>
2023-05-18 14:29:43 +0300

    7-insert_value.sql

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- insert a new row of data in a table in a database
+-- cat 7-insert_value.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
