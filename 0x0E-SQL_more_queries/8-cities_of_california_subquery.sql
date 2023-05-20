3909f79e86241da4d1875f33c70e91c770b40925
LionelMv <mvangicode@gmail.com>
2023-05-21 02:41:39 +0300

    whitespace mess

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -5,10 +5,10 @@
 -- the database name will be passed as an argument of the mysql command
 
 SELECT id, name
-    FROM cities
-    WHERE state_id = (
+FROM cities
+WHERE state_id = (
 	SELECT id
 	FROM states
 	WHERE name = "California"
 	)
-    ORDER BY id ASC;
+ORDER BY id ASC;
