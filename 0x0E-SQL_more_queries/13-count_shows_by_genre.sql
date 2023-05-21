1a7a619bfca5dc3fe37a243ad253fd0a02d2860d
LionelMv <mvangicode@gmail.com>
2023-05-21 22:45:34 +0300

    column naming

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -10,9 +10,9 @@
 -- can use only one SELECT statement
 -- the database name will be passed as an argument of the mysql command
 
-SELECT genre.name, COUNT(*) AS number_shows
-FROM tv_genres AS genre
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
 INNER JOIN tv_show_genres
-    ON genre.id = tv_show_genres.genre_id
-GROUP BY genre.name
-ORDER BY number_shows DESC;
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
