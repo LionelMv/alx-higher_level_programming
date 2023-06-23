20d5b6d16a407137403fbe068c0d4785a90b8839
LionelMv <mvangicode@gmail.com>
2023-06-23 11:10:55 +0300

    added binary to execute

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,15 +10,21 @@ from sys import argv
 if __name__ == "__main__":
 
     # connect to database
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        database=argv[3])
 
     # create cursor to exec queries using SQL; filter names starting with 'N'
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        """SELECT *
+        FROM states
+        WHERE name LIKE BINARY 'N%'
+        ORDER BY id ASC"""
+    )
     rows = cur.fetchall()
 
     for row in rows:
