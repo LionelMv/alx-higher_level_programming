6cbb8c0a9566fac4f7a0b58f74791a571624eec6
LionelMv <mvangicode@gmail.com>
2023-06-23 10:56:37 +0300

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module hat takes in an argument and
+displays all values in the states table
+of the database where name matches the argument.
+"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+
+    # connect database
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        database=argv[3]
+    )
+
+    # create cursor to execute SQL queries
+    cur = db.cursor()
+    cur.execute(
+        """SELECT *
+            FROM states
+            WHERE name LIKE '{:s}'
+            ORDER BY id ASC""".format(argv[4])
+    )
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    # clean up
+    cur.close()
+    db.close()
