c5a556dd6b7b42779dccd2df0c505c6baa186ee4
LionelMv <mvangicode@gmail.com>
2023-06-24 02:30:50 +0300

    changed filter

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     )
     Session = sessionmaker(bind=engine)
     session = Session()
-    state_obj = session.query(State).filter_by(name=argv[4]).first()
+    state_obj = session.query(State).filter_by(State.name == argv[4]).first()
     if state_obj:
         print(f"{state_obj.id}")
     else:
