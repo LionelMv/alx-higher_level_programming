d6a49ca958bfb22870264bc8a3308e5d5c53df92
LionelMv <mvangicode@gmail.com>
2023-06-30 01:04:17 +0300

    added metadata

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -17,6 +17,7 @@ if __name__ == "__main__":
             argv[1], argv[2], argv[3]
         )
     )
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     state_obj = session.query(State).filter_by(State.name == argv[4]).first()
