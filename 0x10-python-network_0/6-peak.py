c608a74b5311ec02a0d2989117fd6baa565c6507
LionelMv <mvangicode@gmail.com>
2023-07-06 01:53:50 +0300

    make executable

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,7 @@
-# function that finds a peak in a list of integers
+#!/usr/bin/python3
+"""
+function that finds a peak in a list of integers
+"""
 
 def find_peak(list_of_integers):
     if list_of_integers == []:
