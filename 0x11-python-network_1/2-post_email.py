8b6e35b06aecddc6983aa0a6550fb9c3e8651d02
LionelMv <mvangicode@gmail.com>
2023-07-07 23:54:57 +0300

    remove whitespace

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,7 +4,6 @@ Script that takes in a URL and an email,
 sends POST req to URL, display response body utf-8
 usage: ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
 """
-
 from urllib import request, parse
 from sys import argv
 
