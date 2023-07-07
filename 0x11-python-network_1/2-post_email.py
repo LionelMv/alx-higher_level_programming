c3769a2d39d8548b669c05dfe02db3d8cae8f0f8
LionelMv <mvangicode@gmail.com>
2023-07-07 23:09:45 +0300

    improved documentation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Script that takes in a URL and an email,
-sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
+sends POST req to URL, display response body utf-8
+usage: ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
 """
 
 from urllib import request, parse
