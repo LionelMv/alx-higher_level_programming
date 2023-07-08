ff5a86a2b9a89351452b7c467fda9e6e4c626e30
LionelMv <mvangicode@gmail.com>
2023-07-08 15:21:44 +0300

    added alx to url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ fetch https://intranet.hbtn.io/status; display response
 import requests
 
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print(f"\t- type: {type(r.text)}")
     print(f"\t- content: {r.text}")
