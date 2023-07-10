8030e29a63cf04104bd24f25b1eb066b144cbf17
LionelMv <mvangicode@gmail.com>
2023-07-10 14:21:31 +0300

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+given repo and owner name as argvs, 
+use Github API (documentation in  documentation https://developer.github.com/v3/repos/commits/) 
+to list last 10 commits
+usage: ./100-github_commits.py [github_repo] [github_owner]
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = f"https://api.github.com/repos/{argv[2]}/{argv[1]}/commits"
+    r = requests.get(url)
+    list_commits = r.json()
+    for commit in list_commits[0:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
